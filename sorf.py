3652c28bc455fca104d070440b8ba4ea1f50781c
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2023-03-29 16:02:40 +0200

    add sorf

---
+++ sorf.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+from pathlib import Path
+import shutil
+import tempfile
+import pty
+import datetime
+
+def is_fasta(filename):
+    with open(filename, 'r') as file:
+        first_line = file.readline().strip()
+    return first_line.startswith(">")
+
+def get_default_output_dir():
+    date_str = datetime.date.today().strftime("deepG_sorf_%Y%m%d_1")
+    output_path = Path(date_str)
+    counter = 1
+    while output_path.exists():
+        counter += 1
+        date_str = datetime.date.today().strftime(f"deepG_sorf_%Y%m%d_{counter}")
+        output_path = Path(date_str)
+    return date_str
+
+def is_podman_installed():
+    try:
+        subprocess.run(["podman", "--version"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return True
+    except FileNotFoundError:
+        return False
+        
+def run_podman(input_file, output_dir, use_gpu=False, batch_size=3000, level="binary", verbose=False, by_entry=False, fast=False):
+    if not is_podman_installed():
+        print("Error: Podman is not installed.")
+        print("Please follow the installation instructions at https://podman.io/getting-started/installation.html")
+        return
+
+    input_path = Path(input_file).resolve()
+    output_path = Path(output_dir).resolve()
+
+    # Check if the input file is in the FASTA format
+    if not is_fasta(input_path):
+        print("Error: Input file is not in the FASTA format.")
+        return
+
+    with tempfile.TemporaryDirectory() as temp_data_dir:
+        temp_data_path = Path(temp_data_dir)
+
+        # Copy input file to temporary data directory
+        shutil.copy(input_path, temp_data_path / input_path.name)
+
+        cmd = [
+            "podman", "run", "--rm", "-i",
+            "--runtime", "nvidia" if use_gpu else "runc",
+            "-v", f"{temp_data_path}:/data",
+            "genomenet/sorf",
+            "predict.r",
+            "--batch_size", str(batch_size),
+            "--input", f"/data/{input_path.name}",
+        ]
+    
+
+        if verbose:
+            print("Starting deepG container")
+
+        def read_output(fd):
+            while True:
+                try:
+                    data = os.read(fd, 1024)
+                except OSError:
+                    break
+
+                if not data:
+                    break
+
+                for line in data.decode().splitlines():
+                    print(line)
+
+        # Create a pseudo-terminal and run the command
+        pid, fd = pty.fork()
+
+        if pid == 0:
+            os.execvp(cmd[0], cmd)
+        else:
+            read_output(fd)
+            _, exit_status = os.waitpid(pid, 0)
+
+        if exit_status != 0:
+            print("Error: Container command failed.")
+            success = False
+        else:
+            success = True
+
+        if success:
+            # Create the output directory if it doesn't exist
+            if not output_path.exists():
+                output_path.mkdir(parents=True)
+
+        # Copy output files from temporary data directory to the specified output directory
+        output_files = (temp_data_path / "output").glob("*")
+        for output_file in output_files:
+            if output_file.is_file():
+                if verbose:
+                    print(f"Copying output file '{output_file.name}' to the specified output directory...")
+                shutil.copy(output_file, output_path / output_file.name)
+            elif output_file.is_dir():  # Add this block to handle directories
+                if verbose:
+                    print(f"Copying output directory '{output_file.name}' to the specified output directory...")
+                shutil.copytree(output_file, output_path / output_file.name)
+
+        # Print a finishing message with input and output files information
+        finishing_message = f"deepG container finished successfully.\nInput: '{input_path}', Output directory: '{output_path}'"
+        if verbose:
+            if verbose:
+                print(finishing_message)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run Container with input and output paths",
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog="Example usage:\n"
+               "  ./sorf.py --input /path/to/example.fasta --output /path/to/output_dir\n"
+               "  ./sorf.py --input /path/to/example.fasta --output /path/to/output_dir --gpu\n"
+               "  ./sorf.py --input /path/to/example.fasta --output /path/to/output_dir --verbose"
+    )
+    parser.add_argument("--input", required=True, help="Path to input file")
+    parser.add_argument("--output", default=get_default_output_dir(), help="Path to output directory (default: current date, e.g., 'deepG_sorf_20210312_1')")
+    parser.add_argument("--gpu", action="store_true", help="Use GPU mode")
+    parser.add_argument("--batch_size", type=int, default=3000, help="Batch size for processing (default: 3000)")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose mode for more information during execution")
+
+    args = parser.parse_args()
+    run_podman(args.input, args.output, args.gpu, args.batch_size, args.verbose)
--- sorfs_container/predict.r
+++ sorfs_container/predict.r
@@ -8,7 +8,6 @@ library(ggplot2)
 
 option_list <- list(
   make_option(c("-i", "--input"), type = "character", default = "/data/example.fasta"),
-  make_option(c("-o", "--output"), type = "character", default = "/data/output.tsv"),
   make_option(c("-s", "--step"), type = "integer", default = 1),
   make_option(c("-b", "--batch_size"), type = "integer", default = 32,
 help = "Number of samples processed in one batch [default %default]",
@@ -24,27 +23,27 @@ if (!file.exists("/data/output")) {
   dir.create("/data/output")
 }
 
-model <- keras::load_model_hdf5("/data/model_binary.h5", compile = FALSE)
+model <- keras::load_model_hdf5("model_binary_sorf.h5", compile = FALSE)
 
 bitmap_pred <- function(fasta_df = NULL, fasta_path = NULL, fasta_index = 1, batch_size = 200, 
                         return_summary = FALSE,
                         step = 1, out_names = NULL,
                         char_sequence = NULL, model = NULL, rc = FALSE, range = NULL, 
                         include_label = FALSE, summary_method = "mean") {
-  
+
   if (!is.null(fasta_path)) fasta_df <- microseq::readFasta(fasta_path)
   if (return_summary) stopifnot(summary_method %in% c("mean", "max"))
-  
+
   maxlen <- model$input_shape[[2]]
   num_out_layers <- length(model$outputs)
-  
+
   if (is.null(char_sequence)) {
     k <- fasta_index
     char_sequence <- fasta_df$Sequence[k]
   }
-  
+
   char_len <- nchar(char_sequence)
-  
+
   if (!is.null(range)) {
     stopifnot(length(range) == 2)
     stopifnot(range[1] < range[2])
@@ -54,18 +53,18 @@ bitmap_pred <- function(fasta_df = NULL, fasta_path = NULL, fasta_index = 1, bat
   } else {
     shift_position <- 0
   }
-  
+
   if (nchar(char_sequence) < maxlen) {
     stop("Sequence too short")
   }
-  
+
   start_ind <- seq(1, nchar(char_sequence) - maxlen + 1, by = step)
-  char_sequence_sub <- substr(char_sequence, 
+  char_sequence_sub <- substr(char_sequence,
                               min(start_ind),
                               max(start_ind) + maxlen - 1)
-  
+
   x <- deepG::seq_encoding_label(sequence = NULL,
-                                 maxlen = maxlen, 
+                                 maxlen = maxlen,
                                  vocabulary = c("a", "c", "g", "t"),
                                  start_ind = start_ind,
                                  ambiguous_nuc = "zero",
@@ -73,8 +72,8 @@ bitmap_pred <- function(fasta_df = NULL, fasta_path = NULL, fasta_index = 1, bat
                                  quality_vector = NULL,
                                  use_coverage = FALSE,
                                  max_cov = NULL,
-                                 cov_vector = NULL, 
-                                 n_gram = NULL, 
+                                 cov_vector = NULL,
+                                 n_gram = NULL,
                                  n_gram_stride = 1,
                                  char_sequence = char_sequence_sub)
   
@@ -93,7 +92,7 @@ bitmap_pred <- function(fasta_df = NULL, fasta_path = NULL, fasta_index = 1, bat
     pred_input <- x[index, , ]
     if (length(index) == 1) pred_input <- array(pred_input, dim = c(1, dim(pred_input)))
     
-    pred_tensor <- predict(model, pred_input)
+    pred_tensor <- predict(model, pred_input, verbose = 0)
     l <- list()
     for (k in 1:num_out_layers) {
       l[[k]] <- keras::array_reshape(pred_tensor[[k]], 
@@ -158,21 +157,20 @@ out_names <- c("positive_direction", "negative_direction")
 df <- bitmap_pred(fasta_df = NULL,
                   fasta_path = opt$input,
                   fasta_index = 1,
-                  batch_size = opt$batch_size, 
+                  batch_size = opt$batch_size,
                   return_summary = FALSE,
                   step = opt$step,
                   out_names = out_names,
                   char_sequence = NULL,
                   model = model,
                   rc = FALSE,
-                  range = NULL, # c(1, 2000), 
-                  include_label = FALSE, 
+                  range = NULL,
+                  include_label = FALSE,
                   summary_method = "mean")
 
-head(df)
-
+write.table(df, file = "/data/output/predictions.tsv", sep = "\t", quote = F, row.names = FALSE)
 df2 <- data.frame(position = rep(df$position, 2),
                   conf = c(df$positive_direction, df$negative_direction),
                   direction = rep(c("positive", "negative"), each = nrow(df)))
 
-write.table(df2, file = "/data/output/predictions.tsv", sep = "\t", quote = F, row.names = FALSE)
+write.table(df2, file = "/data/output/predictions_2.tsv", sep = "\t", quote = F, row.names = FALSE)
